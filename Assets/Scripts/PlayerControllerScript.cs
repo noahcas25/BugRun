8a3df4134e21519ff3c5097e023eadc63362ea1e
Noah <Noah_castro25@yahoo.com>
2021-11-03 04:13:54 -0700

    Start of BugRun
    -Imported Assets
    -Player, joystick and camera set up
    -Player script and animators created
    -Textures imported

---
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerScript : MonoBehaviour
+{
+    
+    public int walkSpeed = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Walk();
+    }
+
+    // Function that moves the player at a constant rate
+
+    private void Walk() {
+        transform.position += transform.forward * walkSpeed * Time.deltaTime;
+    }
+}
