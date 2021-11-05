6be1eaff2a351ef508f498fcfc8bf497be537f61
Noah <Noah_castro25@yahoo.com>
2021-11-05 04:38:18 -0700

    Add swipe functionality
    -TouchScript for swipes
    -Food assets & script
    -New Materials
    Adjusted camera and player movement

--- Assets/Scripts/PlayerControllerScript.cs
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class PlayerControllerScript : MonoBehaviour
 {
     
-    public int walkSpeed = 2;
+    public int walkSpeed = 5;
+    public int jumpForce = 5;
+    public Camera camera;
+
+    private Rigidbody rb;
+    private bool canJump = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -19,9 +24,54 @@ public class PlayerControllerScript : MonoBehaviour
         Walk();
     }
 
-    // Function that moves the player at a constant rate
+    // Moves the player at a constant rate based on the walk speed 
 
     private void Walk() {
         transform.position += transform.forward * walkSpeed * Time.deltaTime;
+        camera.transform.position += transform.forward * walkSpeed * Time.deltaTime;
+    }
+
+    public void Move(string direction) {
+
+
+        switch(direction){
+
+            case"Left": 
+
+            if(transform.position.x > -29) {
+                transform.position += transform.right * (float)-1.25; 
+            }
+
+            break;
+
+            case "Right": 
+
+            if(transform.position.x < -26) { 
+                transform.position += transform.right * (float)1.25; 
+            }
+                
+            break;
+
+            case "Up":
+                Jump();
+            break;
+        }
+    }
+
+    // Adds a force to the Player RB to jump 
+
+    public void Jump() {
+        if(canJump){
+            rb.AddForce(0, jumpForce, 0, ForceMode.Impulse);
+            StartCoroutine(JumpTimer());
+        }
+    }
+
+    // Timer that limits the time between jumps
+
+    private IEnumerator JumpTimer() {
+       canJump = false;
+       yield return new WaitForSeconds((float) 1);
+       canJump = true;
     }
 }
---
+++ Assets/Scripts/TouchController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchController : MonoBehaviour
+{
+
+    private Vector2 startPos;
+    private Vector2 currentPos;
+    private Vector2 endPos;
+
+
+    public GameObject player;
+    public float swipeRange;
+    public float tapRange;
+
+    private bool touchStopped = false;
+
+    void Start() {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Swipe();
+    }
+
+    private void Swipe() {
+
+        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) 
+            startPos = Input.GetTouch(0).position;
+
+        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) {
+            currentPos = Input.GetTouch(0).position;
+
+            Vector2 Distance = currentPos - startPos;
+
+            if(!touchStopped) {
+
+                if(Distance.x < -swipeRange) {
+                    player.GetComponent<PlayerControllerScript>().Move("Left");
+                    touchStopped = true;
+                }
+
+                else if(Distance.x > swipeRange) {
+                   player.GetComponent<PlayerControllerScript>().Move("Right");
+                    touchStopped = true;
+                }
+
+                else if(Distance.y > swipeRange) {
+                     player.GetComponent<PlayerControllerScript>().Move("Up");
+                    touchStopped = true;
+                }
+
+                else if(Distance.y < -swipeRange) {
+                    Debug.Log("Down");
+                    touchStopped = true;
+                }
+            }
+        }
+
+
+        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) {
+
+            touchStopped = false;
+            endPos = Input.GetTouch(0).position;
+
+            Vector2 Distance = endPos - startPos;
+
+            if(Mathf.Abs(Distance.x) < tapRange && Mathf.Abs(Distance.y) < tapRange) {
+                Debug.Log("Tap");
+            }
+        }
+    }
+
+
+
+
+
+}
